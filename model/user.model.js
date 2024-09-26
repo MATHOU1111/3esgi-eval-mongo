639c4c68162ba592c5de75d0a76814dd229a3034
ItakuriUchiha <pierrehenri.luczak@gmail.com>
2024-09-26 09:22:17 +0200

    Pierre-henri : Ajout du model des utilisateurs

--- model/user.model.js
+++ model/user.model.js
@@ -3,3 +3,13 @@
  * 
  * Un user doit avoir au minimum : un login (unique) et un mot de passe
  */
+const {Schema, model} = require('mongoose');
+
+const User = new Schema({
+    login: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    email: {type: String}
+}, {versionKey : false}// versionKey : false permet de ne pas avoir le champ __v dans la base de données
+);
+
+module.exports =  model('User',User);
