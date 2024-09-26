07847d6697871fe50fcb744bbebb1d8ba9c1a262
Mathis <mathisdumage0@gmail.com>
2024-09-26 09:53:41 +0200

    model

--- model/comment.model.js
+++ model/comment.model.js
@@ -1,5 +1,15 @@
+const {SchemaType, model} = require('mongoose');
 /**
  * Créer ici le model pour post
  * 
  * Un post doit avoir au minimum : un message, une date, un userId (auteur du post) et un postId
  */
+
+const commentSchema = mongoose.Schema({
+    message: { type: String, required: true },
+    date: { type: Date, required: true },
+    userId: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+    postId: { type: Schema.Types.ObjectId, ref: 'post', required: true }
+});
+
+module.exports = mongoose.model('comment', commentSchema);
