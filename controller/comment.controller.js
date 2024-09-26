5b8995d421e5c3ebae245a266b56a570f738fe37
Mathis <mathisdumage0@gmail.com>
2024-09-26 10:12:15 +0200

    commentaire controller

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -10,13 +10,14 @@ const Comment = require("./../model/comment.model");
  * }
  */
 exports.create = async () => {
-    try{
-        //TODO
-        res.status(201).json(comment);
-    }catch(e){
-        res.status(500).json(e.message);
-    }
-}
+  try {
+    const newComment = new Comment(req.body);
+    await newComment.save();
+    res.status(201).json(newComment);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+};
 
 /**
  * Méthode pour modifier un commentaire
@@ -27,23 +28,32 @@ exports.create = async () => {
  * }
  */
 exports.update = async () => {
-    try{
-        //TODO
-        res.status(200).json({message: "Commentaire mis à jour"});
-    }catch(e){
-        res.status(500).json(e.message);
+  try {
+    const commentToUpdate = await Comment.findById(req.params.id);
+    if (!commentToUpdate) {
+      return res.status(404).json({ message: "Commentaire non trouvé" });
     }
-}
+    commentToUpdate.message = req.body.message;
+    await commentToUpdate.save();
+    res.status(200).json({ message: "Commentaire mis à jour" });
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+};
 
 /**
  * Méthode pour supprimer un commentaire
  * @param id l'id du commentaire à supprimer
  */
 exports.delete = async () => {
-    try{
-        //TODO
-        res.status(200).json({message: "Commentaire supprimé"});
-    }catch(e){
-        res.status(500).json(e.message);
+  try {
+    const commentToDelete = await Comment.findById(req.params.id);
+    if (!comment) {
+      return res.status(404).json({ message: "Commentaire non trouvé" });
     }
-}
+    await commentToDelete.deleteOne();
+    res.status(200).json({ message: "Commentaire supprimé" });
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+};
