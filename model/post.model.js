66d13c7966a9e538eb3f1ab5a2825538ff41f8d9
Mathis <mathisdumage0@gmail.com>
2024-09-26 11:26:11 +0200

    ajout des comments dans le model

--- model/post.model.js
+++ model/post.model.js
@@ -7,6 +7,7 @@ const {Schema, model} = require('mongoose');
 const postSchema = Schema({
     message: {type: String, required: true},
     date: { type: Date, default: Date.now, required: true},
-    userId: {type: Schema.Types.ObjectId, ref: "user", required: true}
+    userId: {type: Schema.Types.ObjectId, ref: "user", required: true},
+    comments : [{type: Schema.Types.ObjectId, ref: "comment"}]
 }, {versionKey: false});
 module.exports = model('post', postSchema);
