7ee815bed86d006cbe4130392d9c7d4ed98f3cc5
willeboisarthur <arthur.willebois@outlook.fr>
2024-09-26 10:48:24 +0200

    Arthur: post controller/model

--- model/post.model.js
+++ model/post.model.js
@@ -3,3 +3,10 @@
  * 
  * Un post doit avoir au minimum : un message, une date, un userId (auteur du post)
  */
+const {Schema,model} = require('mongoose');
+const postSchema = mongoose.Schema({
+    message: {type: string, required: true},
+    date: { type: Date, default: Date.now, required: true},
+    userId: {type: Schema.Types.ObjectId, ref: "user", required: true}
+}, {versionKey: false});
+module.exports = mongoose.model('post', postSchema);
